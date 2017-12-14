542fc2a912573e66c20d5c4f474ffddf2ace28c0
Matthew Chan <matthew.chan2@mail.mcgill.ca>
2017-12-13 22:48:31 -0500

    fixing file paths

--- src/LSTM/LSTM_csv.py
+++ src/LSTM/LSTM_csv.py
@@ -221,7 +221,7 @@ def main():
     # LOAD THE DATA FOR FAKE NEWS
     # ============================
 
-    news_data = "../data/csv/fake_news.csv"
+    news_data = "../../data/csv/fake_news.csv"
 
     print("Loading Fake News data ... ")
 
@@ -241,7 +241,7 @@ def main():
     model.train(combined_x_news, total_examples=model.corpus_count, epochs=model.iter)
     model.save("fakenews_combined_word2vec")
     '''
-    model = Word2Vec.load("../data/word2vec/fakenews_combined_word2vec")
+    model = Word2Vec.load("../../data/word2vec/fakenews_combined_word2vec")
 
     # Set parameters
     vocab_dim = 300
